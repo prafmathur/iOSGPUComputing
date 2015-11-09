26e68a439e4836cbd1763f2a64c8acb162fd103a
Praful Mathur <praful@prafuls-macbook-pro.local>
2015-11-08 22:56:05 -0500

    Initial Commit

---
+++ iOSGPUComputing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSGPUComputing
+//
+//  Created by Praful Mathur on 11/8/15.
+//  Copyright © 2015 Praf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
