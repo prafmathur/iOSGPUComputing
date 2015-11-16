20980da2e07ba3bce00b8ea25e2468463a6bc49d
Praful Mathur <praful@Prafuls-MacBook-Pro.local>
2015-11-16 13:37:25 -0500

    Cleaned it up:

--- iOSGPUComputing/ViewController.swift
+++ iOSGPUComputing/ViewController.swift
@@ -17,22 +17,26 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var picture: UIImageView!
     
+    let k = 8
+
+    
     var device: MTLDevice!
     var commandQueue: MTLCommandQueue!
     var defaultLibrary: MTLLibrary!
     var rgbProgram: MTLFunction!
     var updateMeans: MTLFunction!
+    var means = [UInt8]()
+    var img : CGImage!
+    var pixelData: CFData!
+    var imgdata: UnsafePointer<UInt8>!
+    var lengthOfData: CFIndex!
 
     
     
     @IBAction func process(sender: UIButton) {
         metalImageProc()
-        //kMeansSerial()
-        //        testProc()
-    }
-//    var myvector = [Float](count: 10000000, repeatedValue: 0)
-//    var finalResultArray = [Float](count: 10000000, repeatedValue: 0)
 
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -48,79 +52,7 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-//    func serialComputation() {
-//        let start = CACurrentMediaTime()
-//
-//        for (idx, val) in myvector.enumerate() {
-//            finalResultArray[idx] = 1.0/(1.0 + pow(2.71828,val))
-//        }
-//        
-//        let end = CACurrentMediaTime()
-//        print(end - start)
-//    }
     
-    func kMeansSerial() {
-        let start = CACurrentMediaTime()
-        let img = picture.image?.CGImage!
-        let pixelData = CGDataProviderCopyData(CGImageGetDataProvider(img))
-        let imgdata: UnsafePointer<UInt8> = CFDataGetBytePtr(pixelData)
-        let lengthOfData = CFDataGetLength(pixelData)
-//        let width = Int((picture.image?.size.width)!)
-//        let height = Int((picture.image?.size.height)!)
-        
-        let k = 8
-        var means = [UInt8]()
-        var assignments = [Int](count: lengthOfData/4, repeatedValue: 0)
-        // Initialize k means to random values
-        for _ in 0...k {
-            let randomPixel = Int(arc4random_uniform(UInt32(lengthOfData/4))) * 4
-            means.append(imgdata[randomPixel])
-            means.append(imgdata[randomPixel + 1])
-            means.append(imgdata[randomPixel + 2])
-            means.append(imgdata[randomPixel + 3])
-        }
-        
-        var distances = [Int]()
-        for i in 0...lengthOfData/4 {
-            var R = imgdata[i*4]
-            var G = imgdata[i*4 + 1]
-            var B = imgdata[i*4 + 2]
-            var A = imgdata[i*4 + 3]
-            
-            for j in 0...k {
-                var meanR = imgdata[j*4]
-                var meanG = imgdata[j*4 + 1]
-                var meanB = imgdata[j*4 + 2]
-                var meanA = imgdata[j*4 + 3]
-                var sum = difSq(R, b: meanR) + difSq(G, b: meanG) + difSq(B, b: meanB) + difSq(A, b: meanA)
-                distances.append(sum)
-            }
-
-            var min = 0
-            var minDist = distances[0]
-            for l in 1...distances.count-1 {
-                if distances[l] < minDist {
-                    min = l
-                    minDist = distances[l]
-                }
-            }
-            
-            assignments[i] = min
-        }
-        
-        var serialCounts = [UInt32](count: k, repeatedValue: 0)
-        for i in assignments {
-            serialCounts[Int(i)]++
-        }
-        
-        print(serialCounts)
-        
-        let end = CACurrentMediaTime()
-        print(end - start)
-        
-        
-    }
     
     func difSq(a: UInt8, b:UInt8) -> Int {
         let aInt = Int(a)
@@ -128,18 +60,11 @@ class ViewController: UIViewController {
         return abs(aInt-bInt) * abs(aInt-bInt)
     }
     
-    
-    func metalImageProc() {
-        let start = CACurrentMediaTime()
-        let img = picture.image?.CGImage!
-        let pixelData = CGDataProviderCopyData(CGImageGetDataProvider(img))
-        let imgdata: UnsafePointer<UInt8> = CFDataGetBytePtr(pixelData)
-        let lengthOfData = CFDataGetLength(pixelData)
-//        let width = Int((picture.image?.size.width)!)
-//        let height = Int((picture.image?.size.height)!)
-//        
-        let k = 8
-        var means = [UInt8]()
+    func initMeans() {
+        self.img = picture.image?.CGImage!
+        self.pixelData = CGDataProviderCopyData(CGImageGetDataProvider(img))
+        self.imgdata = CFDataGetBytePtr(pixelData)
+        self.lengthOfData = CFDataGetLength(pixelData)
         // Initialize k means to random values
         for _ in 0...k {
             let randomPixel = Int(arc4random_uniform(UInt32(lengthOfData/4))) * 4
@@ -148,6 +73,11 @@ class ViewController: UIViewController {
             means.append(imgdata[randomPixel + 2])
             means.append(imgdata[randomPixel + 3])
         }
+    }
+    
+    func metalImageProc() {
+        let start = CACurrentMediaTime()
+
         
         var computePipelineFilter: MTLComputePipelineState? = nil
         do {
@@ -237,9 +167,6 @@ class ViewController: UIViewController {
 
         print(means)
         
-        
-        
-        
         for i in 0...8 {
             let updatedMean = meanSums[i]/meanCounts[i/4]
             means[i] = UInt8(updatedMean)
@@ -251,165 +178,5 @@ class ViewController: UIViewController {
 
     }
     
-    
-    
-    struct PixelData {
-        var r: UInt8
-        var g: UInt8
-        var b: UInt8
-        var a: UInt8
-    }
-
-    func imageFromARGB32Bitmap(pixels:[PixelData], width: Int, height: Int)-> UIImage {
-        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
-        let bitmapInfo:CGBitmapInfo = CGBitmapInfo()
-        let bitsPerComponent:Int = 8
-        let bitsPerPixel:Int = 32
-        
-        assert(pixels.count == Int(width * height))
-        
-        var data = pixels // Copy to mutable []
-        let providerRef = CGDataProviderCreateWithCFData(
-            NSData(bytes: &data, length: data.count * sizeof(PixelData))
-        )
-        
-        let cgim = CGImageCreate(
-            width,
-            height,
-            bitsPerComponent,
-            bitsPerPixel,
-            width * Int(sizeof(PixelData)),
-            rgbColorSpace,
-            bitmapInfo,
-            providerRef,
-            nil,
-            true,
-            CGColorRenderingIntent.RenderingIntentDefault
-        )
-        return UIImage(CGImage: cgim!)
-    }
-    
-    func testProc() {
-        let img = picture.image?.CGImage!
-        
-        let pixelData = CGDataProviderCopyData(CGImageGetDataProvider(img))
-        let imgdata: UnsafePointer<UInt8> = CFDataGetBytePtr(pixelData)
-        let lengthOfData = CFDataGetLength(pixelData)
-        var p = [PixelData](count: Int(lengthOfData)/4, repeatedValue: PixelData(r: 0, g: 0, b: 0, a: 0))
-        print(p.count)
-        for idx in 0...p.count - 1 {
-            let pix = idx * 4
-            var rval = UInt8(imgdata[pix])
-            if (rval > 5) {
-                rval = rval - 5
-            }
-            p[idx] = PixelData(r: rval, g: imgdata[pix+1], b: imgdata[pix+2], a: imgdata[pix+3])
-        }
-        picture.image = imageFromARGB32Bitmap(p, width: Int((picture.image?.size.width)!), height: Int(picture.image!.size.height))
-        
-        
-        //
-//        let providerRef = CGDataProviderCreateWithCFData(
-//            NSData(bytes: imgdata, length: lengthOfData)
-//        )
-//        
-//        let width = Int((picture.image?.size.width)!)
-//        let height = Int((picture.image?.size.height)!)
-//        
-//        let bitsPerComponent:Int = k
-//        let bitsPerPixel:Int = 32
-//        let cgim = CGImageCreate(
-//            width,
-//            height,
-//            bitsPerComponent,
-//            bitsPerPixel,
-//            width * 4,
-//            CGColorSpaceCreateDeviceRGB(),
-//            CGBitmapInfo(rawValue: CGImageAlphaInfo.PremultipliedFirst.rawValue),
-//            providerRef,
-//            nil,
-//            true,
-//            CGColorRenderingIntent.RenderingIntentDefault
-//        )
-//        
-//        let newImage = UIImage(CGImage: cgim!)
-//        picture.image = newImage
-
-        
-    }
-    
-//    func metalComputation() {
-//        let start = CACurrentMediaTime()
-//
-//        let device = MTLCreateSystemDefaultDevice()
-//        let commandQueue = device?.newCommandQueue()
-//        let defaultLibrary = device?.newDefaultLibrary()
-//        let commandBuffer = commandQueue?.commandBuffer()
-//        let computeCommandEncoder = commandBuffer?.computeCommandEncoder()
-//
-//        
-//        let sigmoidProgram = defaultLibrary!.newFunctionWithName("sigmoid")
-//        
-//        
-//        var computePipelineFilter: MTLComputePipelineState? = nil
-//        do {
-//          computePipelineFilter  = try device?.newComputePipelineStateWithFunction(sigmoidProgram!)
-//        }
-//        catch {
-//            
-//        }
-//        computeCommandEncoder?.setComputePipelineState(computePipelineFilter!)
-//        let lengthOfVector = sizeofValue(myvector[0]) * myvector.count
-//        let inBuffer = device?.newBufferWithBytes(&myvector, length: lengthOfVector, options: MTLResourceOptions())
-//        computeCommandEncoder?.setBuffer(inBuffer, offset: 0, atIndex: 0)
-//        
-//        var resultdata = [Float](count:myvector.count, repeatedValue: 0)
-//        let outVectorBuffer = device!.newBufferWithBytes(&resultdata, length: lengthOfVector, options: MTLResourceOptions())
-//        computeCommandEncoder!.setBuffer(outVectorBuffer, offset: 0, atIndex: 1)
-//        
-//        let threadsPerGroup = MTLSize(width: 32, height: 1, depth: 1)
-//        let numThreadGroups = MTLSize(width: (myvector.count + 31)/32, height: 1, depth: 1)
-//        computeCommandEncoder?.dispatchThreadgroups(numThreadGroups, threadsPerThreadgroup: threadsPerGroup)
-//        computeCommandEncoder?.endEncoding()
-//        
-//        commandBuffer?.commit()
-//        commandBuffer?.waitUntilCompleted()
-//        
-//        let data = NSData(bytesNoCopy: outVectorBuffer.contents(), length: myvector.count * sizeof(Float), freeWhenDone: false)
-//        
-//        
-//        data.getBytes(&finalResultArray, length:myvector.count * sizeof(Float))
-//        
-//        let end = CACurrentMediaTime()
-//        print(end - start)
-//        
-//    }
-
-    
-    
-    //        let bitsPerComponent:Int = k
-    //        let bitsPerPixel:Int = 32
-    //        let providerRef = CGDataProviderCreateWithCFData(
-    //            NSData(bytes: &resultData, length: lengthOfData)
-    //        )
-    //
-    //
-    //
-    //        let cgim = CGImageCreate(
-    //            width,
-    //            height,
-    //            bitsPerComponent,
-    //            bitsPerPixel,
-    //            width * 4,
-    //            CGColorSpaceCreateDeviceRGB(),
-    //            CGBitmapInfo(),
-    //            providerRef,
-    //            nil,
-    //            true,
-    //            CGColorRenderingIntent.RenderingIntentDefault
-    //        )
-    //
-    //        let newImage = UIImage(CGImage: cgim!)
-    //        picture.image = newImage
 }
 
